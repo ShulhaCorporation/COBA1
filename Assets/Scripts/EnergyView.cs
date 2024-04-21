adbb8a8bd844e5ccd4844e6ec6d0f7a1d1115974
Nazarii Kurei <94959470+nazaaaar@users.noreply.github.com>
2024-04-21 13:15:33 +0300

    Energy View

--- Assets/Scripts/EnergyView.cs
+++ Assets/Scripts/EnergyView.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EnergyView : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetEnergy(0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetEnergy(float energy){
+        if (energy >= 0 && energy <=1 ) 
+            transform.localScale = new Vector3(energy, 1,1);
     }
 }
