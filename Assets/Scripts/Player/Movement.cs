84223a5cf4b33dd24fa48187d50258244833ffc9
Nazarii Kurei <94959470+nazaaaar@users.noreply.github.com>
2024-05-22 19:54:05 +0300

    pushback

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -56,4 +56,6 @@ public class Movement : MonoBehaviour
     {
         this.canFly = canFly;
     }
+
+
 }
--- Assets/Scripts/Player/TouchSpike.cs
+++ Assets/Scripts/Player/TouchSpike.cs
@@ -6,15 +6,25 @@ using UnityEngine;
 public class TouchSpike : MonoBehaviour
 {
     private LifeSystem lifeset;
+    private Rigidbody2D rigidbody;
+
+    [SerializeField]
+    private float knockback;
     void Start()
     {
         lifeset = gameObject.GetComponent<LifeSystem>();
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Deadly")
         { 
             lifeset.AddHp(-1);
+            Transform otherTransform = collision.transform;
+
+            Vector3 differance = otherTransform.position - transform.position;
+            rigidbody.AddForce(differance * knockback);
+            
         }
     }
 }
