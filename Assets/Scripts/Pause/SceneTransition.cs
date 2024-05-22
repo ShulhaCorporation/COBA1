2d2584278cf2d59cdc4a8bff1a18f8feb8a48ee8
ShulhaCorporation <166990864+ShulhaCorporation@users.noreply.github.com>
2024-05-22 18:49:02 +0300

    Меню + фон
    Меню, фон(накладується поверх рівня). Спроба побудувати карту

--- Assets/Scripts/Pause/SceneTransition.cs
+++ Assets/Scripts/Pause/SceneTransition.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SceneManagement;
-public class ToLevelMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using UnityEngine.SceneManagement;
 
-    // Update is called once per frame
-    void Update()
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField]
+    private int sceneId;
+    public void Update()
     {
-        
+        SceneManager.LoadScene(sceneId);
     }
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -17,19 +17,24 @@ public class PlayerInput : MonoBehaviour
     [SerializeField]
     private KeyCode downKey;
 
+    [SerializeField]
+    private KeyCode escape;
+
     public bool IsRightPressed { get; set; } = false;
     public bool IsLeftPressed { get; set; } = false;
     public bool IsDownPressed { get; set; } = false;
     public bool IsFlyPressed { get; set; } = false;
+    public bool IsEscPressed { get; set; } = false;
 
 
 
     // Update is called once per frame
-    void Update()
+   void Update()
     {
         IsRightPressed = Input.GetKey(rightKey);
         IsLeftPressed = Input.GetKey(leftKey);
         IsDownPressed = Input.GetKey(downKey);
         IsFlyPressed = Input.GetKey(flyKey);
+        IsEscPressed = Input.GetKey(escape);
     }
 }
