103a0e89176d3832d8a7921d45d3c14be2ff1d01
Nazarii Kurei <94959470+nazaaaar@users.noreply.github.com>
2024-06-09 11:34:10 +0300

    anim

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -12,6 +13,9 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private float flightSpeed;
 
+    [SerializeField]
+    private Animator animator;
+
     private PlayerInput playerInput;
     private bool canFly = true;
     // Start is called before the first frame update
@@ -21,10 +25,13 @@ public class Movement : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+
+    float previousHeight;
+    private bool isFlying;
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //var moveX = Input.GetAxis("Horizontal");
 
         int moveX = 0;
         if (playerInput.IsRightPressed)
@@ -42,20 +49,39 @@ public class Movement : MonoBehaviour
         {
             moveY += 1;
         }
-       
-        if (playerInput.IsDownPressed) //різке падіння на кнопку s
-        { moveY -= 1;
-            
-        }
-        
-            rigidbody2D.velocity = new Vector3(moveX * speed, moveY * flightSpeed, 0);
-        
 
+        if (playerInput.IsDownPressed) //різке падіння на кнопку s
+        {
+            moveY -= 1;
+
+        }
+
+        rigidbody2D.velocity = new Vector3(moveX * speed, moveY * flightSpeed, 0);
+
+        animator.SetFloat("moveX", moveX);
+        if (moveX != 0)
+        {
+            animator.SetFloat("lastMoveX", moveX);
+        }
+         animator.SetBool("isFlying", isFlying);
     }
+
     public void SetCanFly(bool canFly)
     {
         this.canFly = canFly;
     }
 
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground"){
+            this.isFlying = false;
+        }
+    }
 
+    private void OnCollisionExit2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground"){
+            this.isFlying = true;
+        }
+    }
 }
