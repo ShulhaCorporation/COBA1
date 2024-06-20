62d24356f56dfa86a2c593388c7e672970ede6af
ShulhaCorporation <166990864+ShulhaCorporation@users.noreply.github.com>
2024-06-20 11:48:28 +0300

    Логіка руху
    По циклу, по порядку, випадково

---
+++ Assets/Scripts/Bat/BatRandom.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatRandom : MonoBehaviour
+{
+    [SerializeField]
+    private List<Vector3> points;
+
+    private int index;
+    [SerializeField]
+    private float speed;
+    private Vector3 movement;
+    private Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement = points[index] - transform.position;
+        rigidbody2D.velocity = movement.normalized * speed;
+        ChangeIndex();
+    }
+    void ChangeIndex()
+    {
+        float pointX = points[index].x;
+        float pointY = points[index].y;
+        if (Mathf.Abs(transform.position.x - pointX) < 0.2 && Mathf.Abs(transform.position.y - pointY) < 0.2) //Замість магнітуди, використовується різниця між Х об'єкта та Х точки і різниця між У об'єкта та У точки. Mathf.Abs() дає модуль різниці, щоб ігнорувати мінуси.
+        {
+            index = Random.Range(0, points.Count);
+        }
+    }
+}
+
+
